3a454eb55067ddf0b1c58406dcb1c8d8d3fccc8a
FaiqFawwaz041 <101858968+FaiqFawwaz041@users.noreply.github.com>
2022-03-19 21:16:31 +0700

    Kalkulator sederhana fix
    Memperindah UI

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -3,30 +3,42 @@ using namespace std;
 int main(){
 	float tambah,kurang,kali,bagi,a,b;
 	int pilih;
+	cout<<"====================="<<endl;
 	cout<<"KALKULATOR SEDERHANA"<<endl;
 	cout<<"1. Penjumlahan"<<endl;
 	cout<<"2. Pengurangan"<<endl;
 	cout<<"3. Perkalian"<<endl;
 	cout<<"4. Pembagian"<<endl;
+	cout<<"====================="<<endl;
+	cout<<"\n";
 	cout<<"Masukan Pilihan Anda : ";cin>>pilih;
 	switch (pilih){
 		case 1:
-			cout<<"Masukan bilangan a: ";cin>>a;
+			cout<<"\nMasukan bilangan a: ";cin>>a;
 			cout<<"Masukan bilangan b: ";cin>>b;
 			tambah=a+b;
-			cout<<"Hasil "<<a<<" + "<<b<<" adalah = "<<tambah;
+			cout<<"\nHasil "<<a<<" + "<<b<<" adalah = "<<tambah;
 			break;
 		case 2:
-			cout<<"Masukan bilangan a: ";cin>>a;
+			cout<<"\nMasukan bilangan a: ";cin>>a;
 			cout<<"Masukan bilangan b: ";cin>>b;
 			kurang=a-b;
-			cout<<"Hasil "<<a<<" - "<<b<<" adalah = "<<kurang;
+			cout<<"\nHasil "<<a<<" - "<<b<<" adalah = "<<kurang;
 			break;
 		case 3:
-			cout<<"Masukan bilangan a: ";cin>>a;
+			cout<<"\nMasukan bilangan a: ";cin>>a;
 			cout<<"Masukan bilangan b: ";cin>>b;
 			kali=a*b;
-			cout<<"Hasil "<<a<<" * "<<b<<" adalah = "<<kali;
+			cout<<"\nHasil "<<a<<" * "<<b<<" adalah = "<<kali;
 			break;
+			
+		case 4:
+			cout<<"\nMasukan bilangan a: ";cin>>a;
+			cout<<"Masukan bilangan b: ";cin>>b;
+			bagi=a/b;
+			cout<<"Hasil "<<a<<" / "<<b<<" adalah = "<<bagi;
+			break;
+		
+		}
+		
 	}
-}
